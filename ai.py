2a2b181af21eb242d68651a06ffef402897b7342
Shashank-RC <shashankreddy0402@gmail.com>
2024-09-23 18:24:15 -0400

    working graph and backtracking

--- ai.py
+++ ai.py
@@ -15,131 +15,170 @@
 # Next I needed to start making the map of the enviorment so that we know how to get back if
 # get to a dead end. So when we move to the next node we see were the Agent can move then linkes the current node
 # to all of the nodes that the agent can move to. 
-# 
-
-import random
-
+# Set up  coordinate to track where the agent is using nodes
+#set up a stack for backtracking and direction to avoid immediate backtracking
+# setting up a graph and updating with the visited percepts
+# priotorzing exploring 
+# setup back tracking 
 
 class AI:
     def __init__(self):
-        # Coordinates to know were the AI is in the map
+        
         self.xCoord = 0
         self.yCoord = 0
-        self.StartNode = Node(0,0)
-        self.StartNode.setVisitedToYes()
-        self.currentNode = self.StartNode
-        # Map to see where the agent has been 
-        self.map = [self.StartNode]
-        self.frontier = []
+        self.currentNode = Node(0, 0)
+        self.currentNode.setVisitedToYes()  
+        self.map = [self.currentNode]  
+        self.path_stack = []  
+        self.last_direction = None  
 
     def update(self, percepts):
 
-        # If the agent is on the goal then return 'U'
+        
         if percepts['X'][0] == 'r':
-            return 'U'
+            print("Goal reached! Terminating...")
+            return 'U'  
+       
         
-        #If the goal is in the 'N' dictionary then go North
-        for i in percepts['N']:
-            if i == 'r':
-                self.xCoord += 1
-                return 'N'
-        
-        #If the goal is in the 'S' dictionary then go South
-        for i in percepts['S']:
-            if i == 'r':
-                self.xCoord += -1
-                return 'S'
-        
-        #If the goal is in the 'E' dictionary then go East
-        for i in percepts['E']:
-            if i == 'r':
-                self.yCoord += 1
-                return 'E'
-        
-        #If the goal is in the 'W' dictionary then go West
-        for i in percepts['W']:
-            if i == 'r':
-                self.yCoord += -1
-                return 'W'
-        
+        for direction in ['N', 'S', 'E', 'W']:
+            if 'r' in percepts[direction]:
+                return self.move_in_direction(direction)
 
-        #Sets North node if it exist 
-        if percepts['N'][0] == 'g':
-            if self.currentNode.northNode == None:
-                for node in self.map:
-                    if node.xCoord == (self.currentNode.xCoord + 1) and node.yCoord == (self.currentNode.yCoord):
-                        self.currentNode.setNorthNode(node)
-                        node.setSouthNode(self.currentNode)
-                        break
-                newNode = Node(self.currentNode.xCoord + 1,self.currentNode.yCoord)
-                self.map.append(newNode)
-                self.currentNode.setNorthNode(newNode)
-
-        #Sets South node if it exist 
-        if percepts['S'][0] == 'g':
-            if self.currentNode.southNode == None:
-                for node in self.map:
-                    if node.xCoord == (self.currentNode.xCoord - 1) and node.yCoord == (self.currentNode.yCoord):
-                        self.currentNode.setSouthNode(node)
-                        node.setNorthNode(self.currentNode)
-                        break
-                newNode = Node(self.currentNode.xCoord - 1,self.currentNode.yCoord)
-                self.map.append(newNode)
-                self.currentNode.setSouthNode(newNode)
-                newNode.setNorthNode(self.currentNode)
-
-        #Sets East node if it exist 
-        if percepts['E'][0] == 'g':
-            if self.currentNode.eastNode == None:
-                for node in self.map:
-                    if node.xCoord == (self.currentNode.xCoord) and node.yCoord == (self.currentNode.yCoord + 1):
-                        self.currentNode.setEastNode(node)
-                        node.setWestNode(self.currentNode)
-                        break
-                newNode = Node(self.currentNode.xCoord,self.currentNode.yCoord + 1)
-                self.map.append(newNode)
-                self.currentNode.setEastNode(newNode)
-                newNode.setWestNode(self.currentNode)
         
-        #Sets West node if it exist 
-        if percepts['W'][0] == 'g':
-            if self.currentNode.eastNode == None:
-                for node in self.map:
-                    if node.xCoord == (self.currentNode.xCoord) and node.yCoord == (self.currentNode.yCoord - 1):
-                        self.currentNode.setWestNode(node)
-                        node.setEastNode(self.currentNode)
-                        break
-                newNode = Node(self.currentNode.xCoord,self.currentNode.yCoord - 1)
-                self.map.append(newNode)
-                self.currentNode.setWestNode(newNode) 
-                newNode.setEastNode(self.currentNode)
+        self.update_graph(percepts)
+
         
-        #This is for testing perposes only
-        self.currentNode = self.currentNode.southNode
-        self.currentNode.setVisitedToYes()
-        return 'S'
+        for direction in ['N', 'S', 'E', 'W']:
+            next_node = self.get_neighbor_node(direction)
+            if next_node and not next_node.visited:
+                self.path_stack.append(self.currentNode)  
+                self.last_direction = direction  
+                return self.move_in_direction(direction)
+
+        
+        if self.path_stack:
+            print("Backtracking to previous node...")
+            backtrack_node = self.path_stack.pop()
+            return self.backtrack_to_node(backtrack_node)
+
+        # If no other options are left, default to moving east
+        print("No more moves available, defaulting to move east.")
+        return 'E'
+
+    def move_in_direction(self, direction):
+        target_node = self.get_neighbor_node(direction)
+        if target_node is None:
+            print(f"Creating new node in direction {direction} before moving.")
+            self.create_neighbor_node(direction)  # Ensure node exists before moving
+
+        if direction == 'N':
+            self.xCoord += 1
+            self.currentNode = self.currentNode.northNode
+        elif direction == 'S':
+            self.xCoord -= 1
+            self.currentNode = self.currentNode.southNode
+        elif direction == 'E':
+            self.yCoord += 1
+            self.currentNode = self.currentNode.eastNode
+        elif direction == 'W':
+            self.yCoord -= 1
+            self.currentNode = self.currentNode.westNode
+        
+        if self.currentNode is None:
+            raise ValueError(f"Error: Moved in direction {direction}, but currentNode is None.")
+
+        print(f"Moved {direction}, new position: x={self.xCoord}, y={self.yCoord}")
+        self.currentNode.setVisitedToYes()  # Mark the node as visited
+        return direction
+
+    def backtrack_to_node(self, backtrack_node):
+        if self.currentNode.northNode == backtrack_node:
+            return self.move_in_direction('N')
+        elif self.currentNode.southNode == backtrack_node:
+            return self.move_in_direction('S')
+        elif self.currentNode.eastNode == backtrack_node:
+            return self.move_in_direction('E')
+        elif self.currentNode.westNode == backtrack_node:
+            return self.move_in_direction('W')
+
+    def update_graph(self, percepts):
+        directions = {
+            'N': (1, 0),
+            'S': (-1, 0),
+            'E': (0, 1),
+            'W': (0, -1)
+        }
+        for direction, (dx, dy) in directions.items():
+            if percepts[direction][0] == 'g':  # Ground is walkable
+                neighbor_node = self.find_or_create_node(self.xCoord + dx, self.yCoord + dy)
+                self.link_nodes(direction, neighbor_node)
+
+    def find_or_create_node(self, x, y):
+        for node in self.map:
+            if node.xCoord == x and node.yCoord == y:
+                return node
+        new_node = Node(x, y)
+        self.map.append(new_node)
+        print(f"Created new node at x={x}, y={y}")
+        return new_node
+
+    def create_neighbor_node(self, direction):
+        directions = {
+            'N': (1, 0),
+            'S': (-1, 0),
+            'E': (0, 1),
+            'W': (0, -1)
+        }
+        dx, dy = directions[direction]
+        new_node = self.find_or_create_node(self.xCoord + dx, self.yCoord + dy)
+        self.link_nodes(direction, new_node)
+
+    def link_nodes(self, direction, neighbor_node):
+        if direction == 'N':
+            self.currentNode.setNorthNode(neighbor_node)
+            neighbor_node.setSouthNode(self.currentNode)
+        elif direction == 'S':
+            self.currentNode.setSouthNode(neighbor_node)
+            neighbor_node.setNorthNode(self.currentNode)
+        elif direction == 'E':
+            self.currentNode.setEastNode(neighbor_node)
+            neighbor_node.setWestNode(self.currentNode)
+        elif direction == 'W':
+            self.currentNode.setWestNode(neighbor_node)
+            neighbor_node.setEastNode(self.currentNode)
+
+    def get_neighbor_node(self, direction):
+        if direction == 'N':
+            return self.currentNode.northNode
+        elif direction == 'S':
+            return self.currentNode.southNode
+        elif direction == 'E':
+            return self.currentNode.eastNode
+        elif direction == 'W':
+            return self.currentNode.westNode
+        return None
+
 class Node:
-    def __init__(self,X,Y):
+    def __init__(self, X, Y):
         self.xCoord = X
         self.yCoord = Y
         self.northNode = None
         self.southNode = None
         self.eastNode = None
         self.westNode = None
-        self.visited = "no"
+        self.visited = False
     
-    def setNorthNode(self,node):
+    def setNorthNode(self, node):
         self.northNode = node
     
-    def setSouthNode(self,node):
+    def setSouthNode(self, node):
         self.southNode = node
     
-    def setEastNode(self,node):
+    def setEastNode(self, node):
         self.eastNode = node
     
-    def setWestNode(self,node):
+    def setWestNode(self, node):
         self.westNode = node
     
     def setVisitedToYes(self):
-        self.visited = "yes"
-      
+        self.visited = True
